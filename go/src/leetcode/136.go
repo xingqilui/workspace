96adda7c781bd3c9cf0711128427e4f04fd6b2ff
chenlei <xingqilui@126.com>
2020-05-15 19:59:07 +0800

    136：官方解法，异或实现

--- go/src/leetcode/136.go
+++ go/src/leetcode/136.go
@@ -1,6 +1,6 @@
 package leetcode
 
-//SingleNumber done
+//SingleNumber ...
 func SingleNumber(nums []int) int {
 	var m = map[int]int{}
 
@@ -15,4 +15,14 @@ func SingleNumber(nums []int) int {
 	}
 
 	return 0
+}
+
+//SingleNumber1 官方解法：使用异或运算。a^a=0, a^0=a，异或满足结合律，所以两两异或全部消为0，最终出现一次的数与0异或等于本身
+func SingleNumber1(nums []int) int {
+	var ret int
+	for _, v := range nums {
+		ret ^= v
+	}
+
+	return ret
 }
